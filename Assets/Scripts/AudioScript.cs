546d0367888a9e463f5f9822011cfa4c837f51af
Alex Seezov <seezovcenko@gmail.com>
2017-01-21 18:19:30 +0200

    HomeTask7 Completed

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA;
+
+public class AudioScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject player = GameObject.Find("Player");
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        Debug.Log("Distance to " + gameObject.name + " is " + distance);
+        float volume = 1 - distance/25;
+        GetComponent<AudioSource>().volume = volume;
+    }
+}
